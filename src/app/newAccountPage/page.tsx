e2a1ffe5bbe86e5f3839b3412208226561abcaf7
Sahithi Gorthi <gorthi.sahithi@gmail.com>
2025-08-18 15:02:21 -0400

    3rd Commit (Tutorial)

--- src/app/newAccountPage/page.tsx
+++ src/app/newAccountPage/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link'
 import {useRouter} from 'next/navigation';
-import {useState, useContext} from 'react';
+import {useState} from 'react';
 import { supabase } from '../../../lib/supabaseClient'
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CurrentTime from '@/app/components/currentTime'
 import Sidebar from '@/app/components/sidebar'
 import Background from '@/app/components/background'
 import { useRouter } from 'next/navigation';
-import { BackgroundContext, UsernameContext, PuppyContext } from '@/app/components/context';
+import { BackgroundContext, PuppyContext } from '@/app/components/context';
 import {AccountButton} from '@/app/accountPage/page'
 import Music from '@/app/components/music'
 import Shop from '@/app/components/shop'
@@ -13,9 +13,13 @@ import {Money} from '@/app/components/streak'
 
 export default function Home(){
   const { background} = useContext(BackgroundContext);
-  const {username} = useContext(UsernameContext);
+  const [isOpen, setIsOpen] = useState(false);
   const {image} = useContext(PuppyContext);
   const router = useRouter();
+
+  function handleTutorial(){
+    setIsOpen(!isOpen);
+  }
 
   return(
     <div
@@ -67,13 +71,70 @@ export default function Home(){
           <div className="absolute flex m-4 bottom-23 right-38">
             <Shop/>
           </div>
-          <div className="absolute flex top-0 left-0 --z-10">
+
+          <div className="bg-blue-100 text-blue-300 font-semibold m-4 px-4 py-2 rounded-3xl absolute top-0 left-0 whitespace-nowrap ">
+            <button onClick={handleTutorial}> Tutorial </button>
+            {isOpen &&
+            <div className="z-0 fixed top-0 left-0 flex bg-black opacity-80 my-17 w-1/6 h-1/2 mx-4 rounded-lg text-white font-semibold">
+              <div className="m-2 overflow-y-auto whitespace-normal font-light ">
+                <p className="font-semibold"> Welcome to Focus Pet!  </p> <br/>
+                Here you can find various tools and resources to create your best digital study space! <br/> <br/>
+                In the tutorial, learn how to use a variety of our features!
+                <br/>
+                <br/>
+                <p className="font-semibold"> Tabs  </p>
+                <p> Focus Pet has two tabs, found on the bottom right corner. Switch between your home page and your focus page by navigating between the bottom bar. The home page will have your money and streak display, while the focus page has all the tools needed for a productive study session.</p>
+                <br/>
+                <p className="font-semibold"> Streak  </p>
+                <p> Streak measures how many consecutive days you have studied on the website! Keep studying consistently for higher streaks!</p>
+
+                <br/>
+                <p className="font-semibold"> Money  </p>
+                <p> Money is earned by studying and can be used to purchase various items in the shop! Currency in Focus Pet is denoted with c! </p>
+                 
+                <br/>
+                <p className="font-semibold"> Task List  </p>
+                <p> The task list helps you keep track of what you need to study! Add tasks to your list and check them off as you complete them! You can also add the difficulty or the subject. </p>
+
+                <br/>
+                <p className="font-semibold"> Shop  </p>
+                <p> The shop allows you to purchase various outfits for your pet! Equip the oufits you have bought by pressing Equip once you have bought the outfit! </p>
+
+                <br/>
+                <p className="font-semibold"> Music  </p>
+                <p> Music can be played in the background while you study! Choose from a variety of tracks to help you focus and stay motivated!</p>
+
+                <br/>
+                <p className="font-semibold"> Background  </p>
+                <p> Change the background of your digital study space! Choose from a variety of themes and colors to create a comfortable and inspiring environment for your studies.</p>
+
+                <br/>
+                <p className="font-semibold"> Change Timer  </p>
+                <p> This feature is located in the focus page! Change the duration of the default timer to a study session most comfortable for you. Click on the button to open the menu of timer choices!</p>
+
+                <br/>
+                <p className="font-semibold"> Timer  </p>
+                <p> This feature is located in the focus page! The timer has Start, Reset, and Pause buttons to control your study sessions.</p>
+
+                <br/>
+                <p className="font-semibold"> Quotes  </p>
+                <p> Quotes are motivational phrases that can inspire you during your study sessions! Regenerate for new quotes to help you stay focused and positive!</p>
+
+              </div>
+            </div>
+            }
+          </div>
+
+
+
+          <div className="absolute flex top-0 left-24 --z-10">
             <Streak/>
           </div>
-          <div className="absolute flex z-10 top-0 left-12 m-4 flex-col place-items-center font-semibold text-blue-300 ">
+          <div className="absolute flex z-10 top-0 left-34 m-4 flex-col place-items-center font-semibold text-blue-300 ">
             <Money/>
           </div>
 
+
     </div>
   );
-} 
+}

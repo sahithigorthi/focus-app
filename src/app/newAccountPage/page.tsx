3697b9a7d9a35aeab935d71eaaaa4923c6d12f68
Sahithi Gorthi <gorthi.sahithi@gmail.com>
2025-08-25 23:28:51 -0400

    6.5

--- src/app/newAccountPage/page.tsx
+++ src/app/newAccountPage/page.tsx
@@ -8,10 +8,7 @@ import SignupForm from '@/app/components/login';
 export default function NewAccountPage() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center bg-blue-100">
-      {/* Signup form handles all Supabase logic */}
       <SignupForm />
-
-      {/* Remaining layout/content of the page */}
       <p className="text-sm font-semibold text-blue-500">Have an account?</p>
       <Link href="/accountPage" className="underline text-sm">
         Sign In

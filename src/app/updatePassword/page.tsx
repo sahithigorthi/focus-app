a357a889f0f4a29e84516156f37fa6a79353b6a0
Sahithi Gorthi <gorthi.sahithi@gmail.com>
2025-09-09 16:44:41 -0400

    7.2 Reset Password w/ Go to Login

--- src/app/updatePassword/page.tsx
+++ src/app/updatePassword/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { supabase } from '../../../lib/supabaseClient'
+import Link from 'next/link'
 
 export default function UpdatePasswordPage() {
   const [password, setPassword] = useState('')
@@ -32,7 +33,10 @@ export default function UpdatePasswordPage() {
       setMessage(error.message)
     } else {
       setMessage('✅ Password updated successfully! You can now log in.')
-      setPassword('')
+      setPassword('');
+      <Link href="/accountPage" className="underline text-sm">
+          Go to Login
+      </Link>
     }
   }
 
